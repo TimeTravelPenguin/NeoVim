69131ea6556765cc94d036892ab5a23960cdbe5e
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-04-21 22:01:34 +1000

    Init cleanup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,4 @@
 return {
-  {
-    "alexghergh/nvim-tmux-navigation",
-    config = function()
-      require "configs.tmux-navigation"
-    end,
-  },
-
   -- File formatting
   {
     "stevearc/conform.nvim",
@@ -15,34 +8,14 @@ return {
     end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
   {
     "williamboman/mason.nvim",
     opts = function()
