7a16044ef0973468e4a49a45329da50ffe2ace1b
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-04-22 00:24:56 +1000

    Corrected options use

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,15 +26,11 @@ return {
   -- },
   {
     "williamboman/mason.nvim",
-    opts = function()
-      require "opts.mason"
-    end,
+    opts = require "opts.mason",
   },
 
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function()
-      require "opts.treesitter"
-    end,
+    opts = require "opts.treesitter",
   },
 }
