53bb72324f9c240404272557bfb2d956ad22de3c
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-04-21 21:40:01 +1000

    Changed theme to catppuccin

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,12 +5,13 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "github_light" },
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
